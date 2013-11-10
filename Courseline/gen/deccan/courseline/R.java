456fa56997d102d9120fe70ee7575537ec227fad
vij2288 <vvankate@andrew.cmu.edu>
2013-11-10 16:25:02 -0500

    commit for design submission1

--- Courseline/gen/deccan/courseline/R.java
+++ Courseline/gen/deccan/courseline/R.java
@@ -29,14 +29,14 @@ public final class R {
         public static final int autoCompleteTextView1=0x7f08000a;
         public static final int button1=0x7f080002;
         public static final int button2=0x7f080003;
-        public static final int editText1=0x7f080006;
-        public static final int editText2=0x7f080007;
-        public static final int editText3=0x7f080009;
+        public static final int checkBox1=0x7f080001;
+        public static final int editText1=0x7f080005;
+        public static final int editText2=0x7f080006;
+        public static final int editText3=0x7f080007;
         public static final int editText4=0x7f080008;
-        public static final int imageView1=0x7f080005;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080000;
-        public static final int textView3=0x7f080004;
+        public static final int editText5=0x7f080009;
+        public static final int imageView1=0x7f080000;
+        public static final int textView1=0x7f080004;
     }
     public static final class layout {
         public static final int deccan_courseline_activity_addition=0x7f030000;
@@ -45,7 +45,7 @@ public final class R {
         public static final int deccan_courseline_activity_main=0x7f030003;
         public static final int deccan_courseline_activity_register=0x7f030004;
         public static final int deccan_courseline_activity_search=0x7f030005;
-        public static final int deccan_courseline_activity_subject=0x7f030006;
+        public static final int deccan_courseline_activity_submission=0x7f030006;
     }
     public static final class menu {
         public static final int addition=0x7f070000;
@@ -58,26 +58,30 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int add_c=0x7f050013;
+        public static final int add_note=0x7f050015;
         public static final int app_name=0x7f050000;
-        public static final int c_code=0x7f050010;
-        public static final int d_code=0x7f05000e;
+        public static final int c_code=0x7f050011;
+        public static final int d_code=0x7f05000f;
+        public static final int delete_c=0x7f050014;
         public static final int email_address=0x7f050008;
         public static final int f_name=0x7f05000b;
-        public static final int g_year=0x7f05000d;
+        public static final int g_year=0x7f05000e;
         public static final int hello_world=0x7f050002;
         public static final int l_name=0x7f05000c;
         public static final int logo_desc=0x7f05000a;
         public static final int pwd=0x7f050009;
-        public static final int search=0x7f050011;
+        public static final int re_pwd=0x7f05000d;
+        public static final int search=0x7f050012;
         public static final int sign_in=0x7f050005;
         public static final int sign_up=0x7f050006;
-        public static final int title_activity_addition=0x7f050012;
-        public static final int title_activity_deletion=0x7f050013;
-        public static final int title_activity_home=0x7f050014;
+        public static final int title_activity_addition=0x7f050016;
+        public static final int title_activity_deletion=0x7f050017;
+        public static final int title_activity_home=0x7f050018;
         public static final int title_activity_register=0x7f050003;
         public static final int title_activity_search=0x7f050004;
-        public static final int title_activity_subject=0x7f050015;
-        public static final int u_pic=0x7f05000f;
+        public static final int title_activity_subject=0x7f050019;
+        public static final int u_pic=0x7f050010;
         public static final int welcome=0x7f050007;
     }
     public static final class style {
--- Courseline/src/deccan/courseline/SubmissionActivity.java
+++ Courseline/src/deccan/courseline/SubmissionActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class SubjectActivity extends Activity {
+public class SubmissionActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.deccan_courseline_activity_subject);
+		setContentView(R.layout.deccan_courseline_activity_submission);
 	}
 
 	@Override
