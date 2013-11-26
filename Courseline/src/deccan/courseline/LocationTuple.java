5a0f25e614cd6a62707b8c71caa3f0d354e04cb8
SAGAR M <smedikeri@cmu.edu>
2013-11-25 22:28:10 -0500

    search images

---
+++ Courseline/src/deccan/courseline/LocationTuple.java
@@ -0,0 +1,16 @@
+package deccan.courseline;
+
+public class LocationTuple {
+	public double x;
+	public double y;
+	
+	public LocationTuple() {
+		super();
+	}
+	
+	public LocationTuple(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+}
